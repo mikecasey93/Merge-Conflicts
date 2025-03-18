13515017558fb6a5d6fb58d6f58d9c3582d2ff35
Michael Casey <36479830+mikecasey93@users.noreply.github.com>
2025-03-17 21:10:04 -0400

    initial commit

---
+++ app/src/main/java/com/example/mergeconflict/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.mergeconflict
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mergeconflict.ui.theme.MergeConflictTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MergeConflictTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Michael",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+    Text(text = "This is practice for resolving merge conflicts")
+    Text(text = "This is the first initial commit")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MergeConflictTheme {
+        Greeting("Android")
+    }
+}
